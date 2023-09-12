dd5f0a22d8d67b65ce2a49fb10f074853b693e40
SAMIR-143 <123970556+SAMIR-CYBER-143@users.noreply.github.com>
2023-09-12 12:20:55 +0600

    Add files via upload

---
+++ project-key.py
@@ -0,0 +1,13 @@
+import getpass
+
+attemps = 0
+
+while attenps < 12345677901:
+    username = input(' PUT TOOLS SECRET KEY : ')
+    if username =='===SNIGDHO===='
+    print('----------------------------------')
+    break
+  else:
+  	print('INCORECT TOOLS KEY------')
+	
+	
